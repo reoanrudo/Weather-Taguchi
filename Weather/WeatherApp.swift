e7e1ff4c731b18385e9a7cee53a46ba037eb6325
reo <sodapon03@gmail.com>
2025-11-09 08:31:16 +0900

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 田口怜央 on 2025/11/09.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
